c712c684f5615c6751fe0ba01e5f0d122381a87b
AmirHossein Roozbahani <ahrzb5@gmail.com>
2022-04-18 14:20:49 +0200

    Move gitconfigs under nix

--- darwin-dh/home/home.nix
+++ darwin-dh/home/home.nix
@@ -4,6 +4,31 @@
   home.sessionVariables = { EDITOR = "vim"; };
 
   programs = {
+    git = {
+      enable = true;
+      aliases = {
+        co = "checkout";
+        lg = "log --oneline --decorate --graph";
+      };
+      extraConfig = {
+        core.excludesfile = ./dotfiles/git/global.gitignore;
+      };
+      includes = [
+        {
+          path = ./dotfiles/git/deliveryhero.gitconfig;
+          condition = "gitdir:~/repos/github.com/deliveryhero/**";
+        }
+        {
+          path = ./dotfiles/git/personal.gitconfig;
+          condition = "gitdir:~/repos/github.com/ahrzb/**";
+        }
+        {
+          path = ./dotfiles/git/personal.gitconfig;
+          condition = "gitdir:~/repos/codeberg.org/ahrzb/**";
+        }
+      ];
+    };
+
     direnv = {
       enable = true;
       nix-direnv = { enable = true; };
