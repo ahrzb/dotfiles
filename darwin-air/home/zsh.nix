b4fe324d6cd2c257778ba78e303831c0dbd4abc8
AmirHossein Roozbahani <ahrzb5@gmail.com>
2022-05-11 01:18:20 +0200

    darwin-air(zsh): Migrate to zinit, seems faster

--- darwin-air/home/zsh.nix
+++ darwin-air/home/zsh.nix
@@ -1,72 +1,32 @@
-{ config, pkgs, lib, ... }: {
+{ pkgs, ... }:
+let zinit_home = pkgs.fetchFromGitHub {
+  owner = "zdharma-continuum";
+  repo = "zinit";
+  rev = "c703290fc22839bc799790798460bbe850ce9688";
+  sha256 = "sha256-QWo4WX3yBO3oOs9DkhYCUB8RjfqPFZwifw4VId1TXUQ=";
+};
+in
+{
   programs.zsh = {
     enable = true;
-    zplug = {
-      enable = true;
-      plugins = [
-        {
-          name = "plugins/zsh-interactive-cd";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/git";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/aws";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/pip";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/brew";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/python";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/golang";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/emacs";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/vim";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/iterm2";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/vagrant";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/docker";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/docker-compose";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/kubectl";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "plugins/z";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        { name = "zsh-users/zsh-syntax-highlighting"; }
-        { name = "changyuheng/fz"; }
-      ];
-    };
+    initExtraFirst = "zmodload zsh/zprof";
+    initExtra = "zprof";
+    initExtraBeforeCompInit = ''
+      export ZINIT_HOME="${zinit_home}"
+      source "${zinit_home}/zinit.zsh"
+      zinit snippet OMZ::plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
+      zinit snippet OMZ::plugins/git/git.plugin.zsh
+      zinit snippet OMZ::plugins/aws/aws.plugin.zsh
+      zinit snippet OMZ::plugins/brew/brew.plugin.zsh
+      zinit snippet OMZ::plugins/pip/pip.plugin.zsh
+      zinit snippet OMZ::plugins/python/python.plugin.zsh
+      zinit snippet OMZ::plugins/golang/golang.plugin.zsh
+      zinit snippet OMZ::plugins/iterm2/iterm2.plugin.zsh
+      zinit snippet OMZ::plugins/vagrant/vagrant.plugin.zsh
+      zinit snippet OMZ::plugins/docker/docker.plugin.zsh
+      zinit snippet OMZ::plugins/docker-compose/docker-compose.plugin.zsh
+
+      zinit load "zsh-users/zsh-syntax-highlighting"
+    '';
   };
 }
