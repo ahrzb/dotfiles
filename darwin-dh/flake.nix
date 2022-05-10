87870a14c47da47d1b7547e36c2b28624ce65ff4
AmirHossein Roozbahani <ahrzb5@gmail.com>
2022-05-10 22:37:10 +0200

    darwin-dh: Improve naming and formatting

--- darwin-dh/flake.nix
+++ darwin-dh/flake.nix
@@ -11,11 +11,11 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
-    nixvim.url = github:pta2002/nixvim;
+    nixvim.url = "github:pta2002/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs-unstable";
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager, ... }@inputs:
+  outputs = { darwin, home-manager, ... }@inputs:
     let
       inherit (darwin.lib) darwinSystem;
       inherit (inputs.nixpkgs-unstable.lib) attrValues optionalAttrs singleton;
@@ -24,12 +24,13 @@
         overlays = attrValues overlays;
       };
       overlays = {
-        extra-pkgs = final: prev: {
+        extra-pkgs = _final: _prev: {
           pkgs-stable =
             import inputs.nixpkgs { inherit (nixpkgsConfig) config; };
         };
       };
-    in {
+    in
+    {
       darwinConfigurations = {
         "C02G108AML86" = darwinSystem {
           system = "x86_64-darwin";
