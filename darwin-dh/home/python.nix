011aa37c5e8ecb98345e967046c5a2e3e7dc8f6d
AmirHossein Roozbahani <ahrzb5@gmail.com>
2022-06-14 14:11:51 +0200

    darwin-dh(python): Upgrade to Python 3.10

--- darwin-dh/home/python.nix
+++ darwin-dh/home/python.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }: {
+{ pkgs, lib, ... }: {
   home.packages = lib.singleton
-    (pkgs.python39.withPackages (ps: with ps; [ ipython pyaml GitPython ]));
+    (pkgs.python310.withPackages (ps: with ps; [ ipython pyaml GitPython ]));
 }
