349adfd20a9df6101c9c6f7c9775597c00934036
AmirHossein Roozbahani <ahrzb5@gmail.com>
2023-06-27 14:37:36 +0200

    ubuntu-dh: Update nix flake, add emacs overlay

--- ubuntu-dh/flake.nix
+++ ubuntu-dh/flake.nix
@@ -6,17 +6,27 @@
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    emacs-overlay.url = "github:nix-community/emacs-overlay/";
+    emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
   let
     home = inputs.home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      modules = [ ./home.nix ./packages.nix ./zsh.nix ./bash.nix ./python.nix ./modules ];
+      modules = [
+        ./home.nix ./packages.nix ./zsh.nix ./bash.nix ./python.nix ./modules
+        {
+          nixpkgs.overlays = [
+            (import inputs.emacs-overlay)
+          ];
+        }
+      ];
     };
   in {
     packages.x86_64-linux.homeConfigurations = home;
-    apps.x86_64-linux.default = {
+    apps.x86_64-linux.activate = {
       type = "app";
       program = "${home.activationPackage}/activate";
     };
