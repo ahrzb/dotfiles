02cd672157917a2dd1d074a8c614cfd82fdc6213
AmirHossein Roozbahani <ahrzb5@gmail.com>
2023-11-01 13:50:33 +0100

    ubuntu-dh(bash): Remove qfc from bash

--- ubuntu-dh/bash.nix
+++ ubuntu-dh/bash.nix
@@ -1,12 +1,4 @@
-{ pkgs, config, ... }:
-let
-  qfc_root = pkgs.fetchFromGitHub {
-    owner = "pindexis";
-    repo = "qfc";
-    rev = "3ed9e530af70a1ce9c6b23abb10c5f7617217494";
-    sha256 = "sha256-Ir30u9mGEEaF7iBbxu0cn0Q+07DmkpKnNnLJdOy7R58=";
-  };
-in {
+{ config, ... }: {
   programs.bash = {
     enable = true;
 
@@ -16,18 +8,22 @@ in {
       fi
 
       if [ -d "${config.home.homeDirectory}/bin" ] ; then
-          PATH="$HOME/bin:$PATH"
+          export PATH="$HOME/bin:$PATH"
       fi
 
       if [ -d "${config.home.homeDirectory}/.local/bin" ] ; then
-          PATH="$HOME/.local/bin:$PATH"
+          export PATH="$HOME/.local/bin:$PATH"
       fi
     '';
 
     initExtra = ''
       . "${config.xdg.configHome}/asdf-direnv/bashrc"
-      . "${qfc_root}/bin/qfc.sh"
+
       eval "$(register-python-argcomplete pipx)"
+
+      if [ -f ~/.secrets ]; then
+        . ~/.secrets
+      fi
     '';
   };
 }
