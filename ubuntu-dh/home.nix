4d836b0dcacc14c18d5132d6e499ce1cdd524412
AmirHossein Roozbahani <ahrzb5@gmail.com>
2023-11-27 11:26:25 +0100

    ubuntu-dh: Clean up programs section

--- ubuntu-dh/home.nix
+++ ubuntu-dh/home.nix
@@ -13,37 +13,10 @@
     home-manager.enable = true;
     git.enable = true;
     zsh.enable = true;
-
-    direnv = {
-      enable = true;
-      nix-direnv = { enable = true; };
-    };
-
-    htop = {
-      enable = true;
-      settings = { show_program_path = true; };
-    };
-
-    starship = {
-      enable = true;
-      settings = {
-        aws.style = "bold #ffb86c";
-        cmd_duration.style = "bold #f1fa8c";
-        directory.style = "bold #50fa7b";
-        hostname.style = "bold #ff5555";
-        git_branch.style = "bold #ff79c6";
-        git_status.style = "bold #ff5555";
-        username = {
-          format = "[$user]($style) on ";
-          style_user = "bold #bd93f9";
-        };
-        character = {
-          success_symbol = "[λ](bold #f8f8f2)";
-          error_symbol = "[λ](bold #ff5555)";
-        };
-      };
-    };
-
+    neovim.enable = true;
+    awscli.enable = true;
+    htop.enable = true;
+    starship.enable = true;
     zoxide.enable = true;
     skim.enable = true;
     bat.enable = true;
@@ -52,19 +25,16 @@
     go.enable = true;
     jq.enable = true;
     k9s.enable = true;
+    ssh.enable = true;
 
     eza = {
       enable = true;
       enableAliases = true;
     };
 
-    ssh = {
+    direnv = {
       enable = true;
-      matchBlocks."*" = {
-        host = "*";
-        extraOptions.IdentityAgent = "~/.1password/agent.sock";
-        extraOptions.AddKeysToAgent = "true";
-      };
+      nix-direnv = { enable = true; };
     };
   };
 
