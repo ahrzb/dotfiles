92d31399480df3806b5dd5d5226127ffeaf54127
AmirHossein Roozbahani <ahrzb5@gmail.com>
2025-01-22 13:49:16 +0100

    ubuntu-dh(zsh): Add helper functions

--- ubuntu-dh/home.nix
+++ ubuntu-dh/home.nix
@@ -13,6 +13,25 @@
     home-manager.enable = true;
     git.enable = true;
     zsh.enable = true;
+    zsh.initExtra = ''
+      function gdp-access() {
+        eval "$(dp-devinfra destinations access --export-env-vars "$1" "$2" "$3")"
+      }
+
+      function dhlogin() {
+        mfa="`op item get 54xiseagtbwviz2cyvfm4ppzza --otp`"
+
+        saml2aws login -a log-prd --mfa="TOTP" --mfa-token="$mfa" --force --skip-prompt && \
+        saml2aws login -a log-stg --mfa="TOTP" --mfa-token="$mfa" --force --skip-prompt && \
+        saml2aws login -a log-shared --mfa="TOTP" --mfa-token="$mfa"  --force --skip-prompt && \
+        saml2aws login -a log-dwh-prd --mfa="TOTP" --mfa-token="$mfa" --force --skip-prompt && \
+        saml2aws login -a log-prd --mfa="TOTP" --profile gdp-log-prd --mfa-token="$mfa" --force --skip-prompt && \
+        saml2aws login -a log-stg --mfa="TOTP" --profile gdp-log-stg --mfa-token="$mfa" --force --skip-prompt && \
+        aws --region eu-west-1 ecr get-login-password --profile log-prd | docker login --username AWS --password-stdin "683110685365.dkr.ecr.eu-west-1.amazonaws.com" && \
+        aws --region eu-west-1 ecr get-login-password --profile log-stg | docker login --username AWS --password-stdin "940776968316.dkr.ecr.eu-west-1.amazonaws.com" && \
+        aws --region eu-west-1 ecr get-login-password --profile log-stg | docker login --username AWS --password-stdin "913464273150.dkr.ecr.eu-west-1.amazonaws.com"
+      }
+    '';
     neovim.enable = true;
     htop.enable = true;
     starship.enable = true;
