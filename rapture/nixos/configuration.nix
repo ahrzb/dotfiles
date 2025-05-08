d37f55f96c72ff8df85eb40fa0a43934c8917f4b
AmirHossein Roozbahani <ahrzb5@gmail.com>
2025-05-08 11:43:57 +0200

    rapture(nixos): Enable containers

--- rapture/nixos/configuration.nix
+++ rapture/nixos/configuration.nix
@@ -222,4 +222,19 @@
   fonts.packages = with pkgs; [
     nerd-fonts.fira-code
   ];
+
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    docker = {
+      enable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+    };
+    podman = {
+      enable = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
 }
